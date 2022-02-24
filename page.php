90d09a7ccb8cdf00cc0e078b10c14f140502ee00
ttshahr <ttshahr@gmail.com>
2022-02-24 23:47:05 +0330

    Update page.php

--- page.php
+++ page.php
@@ -7,7 +7,7 @@
 		<div class="content">
 
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<div class="post" id="post-<?php the_ID(); ?>">
+		<div class="post" id="page-id-<?php the_ID(); ?>">
 		<h2><?php the_title(); ?></h2>
 			<div class="entry">
 				<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
